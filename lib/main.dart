3eb09077a4612bc92ffc533d8674fc91ceed6086
= <ivn.mza@gmail.com>
2023-03-29 03:09:28 -0700

    deleted test video item

--- lib/main.dart
+++ lib/main.dart
@@ -152,60 +152,6 @@ class _VideoItemState extends State<VideoItem> {
   }
 }
 
-class TestVideoPlayerItem extends StatefulWidget {
-  const TestVideoPlayerItem({super.key});
-
-  @override
-  State<TestVideoPlayerItem> createState() => _TestVideoPlayerItemState();
-}
-
-class _TestVideoPlayerItemState extends State<TestVideoPlayerItem> {
-  late VideoPlayerController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.network(
-        'https://firebasestorage.googleapis.com/v0/b/thrivebynightdev-866d3.appspot.com/o/2023-03-22-21-11-18.mov?alt=media&token=42fcf4de-ccdf-4d32-89df-137789fd0428',
-        videoPlayerOptions: VideoPlayerOptions(mixWithOthers: true),);
-    _controller.addListener(() {
-      setState(() {});
-    });
-    _controller.setLooping(true);
-    _controller.initialize();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(padding: const EdgeInsets.only(top: 20.0)),
-        const Text('With remote mp4'),
-        Container(
-          padding: const EdgeInsets.all(20),
-          child: AspectRatio(
-            aspectRatio: _controller.value.aspectRatio,
-            child: Stack(
-              alignment: Alignment.bottomCenter,
-              children: <Widget>[
-                VideoPlayer(_controller),
-                ClosedCaption(text: _controller.value.caption.text),
-                _ControlsOverlay(controller: _controller),
-                VideoProgressIndicator(_controller, allowScrubbing: true),
-              ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
 
 class _ControlsOverlay extends StatelessWidget {
   const _ControlsOverlay({required this.controller});
