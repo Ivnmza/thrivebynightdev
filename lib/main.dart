c5850346f897203fd136ef31fec6c2e88bee0daa
= <ivn.mza@gmail.com>
2023-10-17 14:12:43 -0700

    remove chewie references

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:logger/logger.dart';
 import 'package:video_player/video_player.dart';
 import 'firebase_options.dart';
 import 'service/storage_service.dart';
-import 'package:chewie/chewie.dart';
 
 var logger = Logger();
 
@@ -62,22 +61,12 @@ class _MyHomePageState extends State<MyHomePage> {
     logger.d(vidList);
   }
 
-  // approach #2
-  void getCloudVidURLs() async {
-    cloudVideoList = await storage.getCloudVideos();
-  }
-// approach #3
-  Future<List<CloudVideo>> _cloudVideos() async {
-    List<CloudVideo> cloudVideoList = await storage.getCloudVideos();
-    return cloudVideoList;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Thrivebynightdev")),
         body: FutureBuilder(
-          future: _cloudVideos(),
+          future: storage.getCloudVideos(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               if (snapshot.connectionState == ConnectionState.waiting) {
@@ -108,78 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class ChewieItem extends StatefulWidget {
-  const ChewieItem({super.key, required this.url, required this.title});
-  final String url;
-  final String title;
 
-  @override
-  State<ChewieItem> createState() => _ChewieItemState();
-}
-
-class _ChewieItemState extends State<ChewieItem> {
-  late VideoPlayerController _ccontroller;
-  late ChewieController _chewieController;
-  int? bufferDelay;
-
-  @override
-  void initState() {
-    super.initState();
-    initializePlayer();
-    _chewieController = ChewieController(
-      videoPlayerController: _ccontroller,
-      autoPlay: true,
-      looping: true,
-      progressIndicatorDelay:
-          bufferDelay != null ? Duration(milliseconds: bufferDelay!) : null,
-      subtitleBuilder: (context, dynamic subtitle) => Container(
-        padding: const EdgeInsets.all(10.0),
-        child: subtitle is InlineSpan
-            ? RichText(
-                text: subtitle,
-              )
-            : Text(
-                subtitle.toString(),
-                style: const TextStyle(color: Colors.black),
-              ),
-      ),
-      hideControlsTimer: const Duration(seconds: 1),
-    );
-  }
-
-  @override
-  void dispose() {
-    _ccontroller.dispose();
-    _chewieController.dispose();
-    super.dispose();
-  }
-
-  Future<void> initializePlayer() async {
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: _chewieController.videoPlayerController.value.isInitialized
-              ? Chewie(
-                  controller: _chewieController,
-                )
-              : const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircularProgressIndicator(),
-                    SizedBox(height: 20),
-                    Text('Loading'),
-                  ],
-                ),
-        ),
-      ],
-    );
-  }
-}
 
 class BlogPostItem extends StatefulWidget {
   final String title;
